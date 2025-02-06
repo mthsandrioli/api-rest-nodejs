c85a61aab47085d09cbd50f2636e837ecde5f737
mthsandrioli <matheus_andrioli@hotmail.com>
2025-02-06 12:44:27 -0300

    feat: fastify plugins implements

---
+++ src/routes/transactions.ts
@@ -0,0 +1,17 @@
+import crypto from 'node:crypto'
+import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/hello', async () => {
+    const transaction = await knex('transactions')
+      .insert({
+        id: crypto.randomUUID(),
+        title: 'Transação de teste',
+        amount: 1000,
+      })
+      .returning('*')
+
+    return transaction
+  })
+}
